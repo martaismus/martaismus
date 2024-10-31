06569512736878284993e3014b315701ad58acd7
Jan Čech <jan.cech@osc.cz>
2024-10-31 17:27:56 +0100

    feat: gallery optimalization

--- gallery.js
+++ gallery.js
@@ -3,9 +3,9 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg")
 
-function openFullImg(pic){
+async function openFullImg(pic){
     fullImgBox.style.display = "flex";
-    fullImg.src = pic;
+    fullImg.src = await pic.replace("Optimized", 'FullSize');
 }
 
 function closeFullImg(){
